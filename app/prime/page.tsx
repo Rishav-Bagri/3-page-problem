f09e580e91961e8d7d00bcf957ab3078d116e126
Rishav <bagririshav01@gmail.com>
2026-06-01 11:14:19 +0530

    await connection

--- app/prime/page.tsx
+++ app/prime/page.tsx
@@ -1,4 +1,5 @@
 import Game from "@/components/Game";
+import { connection } from "next/server";
 const validTargets: number[] = [
   8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30, 
   32, 33, 34, 35, 36, 38, 39, 40, 42, 44, 45, 46, 48, 49, 50, 51, 52, 
@@ -6,7 +7,8 @@ const validTargets: number[] = [
   76, 77, 78, 80, 81, 82, 84, 85, 86, 87, 88, 90, 91, 92, 93, 94, 95, 
   96, 98, 99
 ];
-export default function About() {
+export default async function About() {
+    await connection()
     const randomIndex = Math.floor(Math.random() * validTargets.length)
   const initialTarget = validTargets[randomIndex]
   return <div>
